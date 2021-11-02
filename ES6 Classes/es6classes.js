d0dd3c62440eccd1731a4e49ea714b94e11cc5e7
SilMontes <Silmontes2001@gmail.com>
2021-11-02 02:03:44 +0000

    ES6

---
+++ ES6 Classes/es6classes.js
@@ -0,0 +1,12 @@
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    //methods are define in the body
+    draw(){
+        console.log('draw')
+    }
+}
+
+const c = new Circle(2);
